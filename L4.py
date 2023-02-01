00921ea10735e59f4443a93701a21244ae706aed
Gabby F G <123026505+GFGallaway@users.noreply.github.com>
2023-02-01 16:00:54 -0500

    Add files via upload

---
+++ L4.py
@@ -0,0 +1,10 @@
+# authorr's names are Gabby and Saerim
+import math
+r=4
+circle=math.pi*r**2
+print("Area of circle is", circle)
+l=8/math.sqrt(2)
+square=l*l
+print("Area of square is", square)
+y=circle-square
+print("Area between them is", y)
